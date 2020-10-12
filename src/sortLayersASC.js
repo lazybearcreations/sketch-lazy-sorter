d5dd93db888a8463d36322919b7214984ae6cdce
Adam Bruzon <adam.bruzon@centrica.com>
2020-10-12 18:11:32 +0100

    Initial commit version 1

---
+++ src/sortLayersASC.js
@@ -0,0 +1,30 @@
+import { message } from 'sketch/ui';
+import { Document } from 'sketch/dom';
+
+export default function() {
+
+  const document = Document.getSelectedDocument();
+  const selectedPage = document.selectedPage;
+
+  let names = selectedPage.layers.map(layer => layer.name);
+      names.sort();
+
+  for (var i = 0; i < names.length; i++) {
+
+    var layers = document.getLayersNamed(names[i]);
+
+    // Get only from selected Page
+    for (var n = 0; n < layers.length; n++) {
+      if (layers[n].getParentPage().selected) {
+        layers = layers[n];
+        break;
+      }
+    }
+
+    layers.index = (names.length - 1) - i;
+
+  }
+
+  message("Layers sorted ascending 🙌");
+
+}
